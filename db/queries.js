2ce33c885b24044ba8c1defbdac42e6560ecb3c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-03 18:13:38 +0200

    feat: add getUser query

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,11 @@
 const pool = require('./pool');
+
+async function getUser(email) {
+  const { rows } = await pool.query('SELECT * FROM users WHERE email = $1', [
+    email,
+  ]);
+  const user = rows[0];
+  return user;
+}
+
+module.exports = { getUser };
