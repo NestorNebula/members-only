8d9af8e99a3ac7bf5f85f759a25a68bc6d609302
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-03 17:22:33 +0200

    feat: add session configuration in app.js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('node:path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const pgSession = require('connect-pg-simple')(session);
+const pool = require('./db/pool');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    store: new pgSession({
+      pool: pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+  })
+);
+app.use(passport.session());
 
 const PORT = process.env.PORT;
 app.listen(PORT);
