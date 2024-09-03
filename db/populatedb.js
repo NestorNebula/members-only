23b1f3d919cdefe8a7e96371cd6d8f0b0c7ca325
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-03 11:40:10 +0200

    feat: add script to create db tables

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,7 +1,25 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(25) NOT NULL,
+  last_name VARCHAR(25) NOT NULL,
+  email VARCHAR(50) UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  member BOOLEAN DEFAULT false,
+  admin BOOLEAN DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(50) NOT NULL,
+  timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  content VARCHAR(255) NOT NULL,
+  user_id INTEGER REFERENCES users(id)
+);
+`;
 
 async function main() {
   const client = new Client({
