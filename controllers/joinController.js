8cd651b281f90d7d34dab6379767c64881d7fe0d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-04 16:19:43 +0200

    feat: add "form" to become a member

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const db = require('../db/queries');
+
 const getJoin = (req, res) => {
   if (!req.user) {
     return res.redirect('/');
@@ -5,8 +8,12 @@ const getJoin = (req, res) => {
   res.render('join');
 };
 
-const postJoin = (req, res) => {
-  res.redirect('/');
+const postJoin = async (req, res) => {
+  if (req.body.password === process.env.CLUB_PASSWORD) {
+    await db.updateMemberStatus(req.user);
+    return res.redirect('/');
+  }
+  res.render('join', { error: 'This is not the expected password.' });
 };
 
 module.exports = { getJoin, postJoin };
