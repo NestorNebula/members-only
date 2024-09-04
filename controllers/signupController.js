ea442ff082061f1fd27ad9d932335466fd1add15
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-04 11:07:37 +0200

    feat: check that email isn't taken for new user

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,4 +1,5 @@
 const { body, validationResult } = require('express-validator');
+const db = require('../db/queries');
 
 const errNameLength = 'should have a length between 2 and 25 characters.';
 const errEmailLength = 'Email should have a maximum length of 50 characters.';
@@ -13,14 +14,21 @@ const userValidation = [
   body('last_name')
     .trim()
     .blacklist('<>')
-    .isLength({ min: 2, max: 25 }.withMessage(`Last name ${errNameLength}`)),
+    .isLength({ min: 2, max: 25 })
+    .withMessage(`Last name ${errNameLength}`),
   body('email')
     .trim()
     .blacklist('<>')
     .isLength({ max: 50 })
     .withMessage(errEmailLength)
     .isEmail()
-    .withMessage(errEmail),
+    .withMessage(errEmail)
+    .custom(async (email) => {
+      const user = await db.getUser(email);
+      if (user) {
+        throw new Error('This Email is already taken.');
+      }
+    }),
 ];
 
 function getSignup(req, res) {
@@ -32,6 +40,7 @@ const postSignUp = [
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.log(errors);
       return res.render('sign-up', { errors: errors.array() });
     }
     res.redirect('/');
