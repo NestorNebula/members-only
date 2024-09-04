372e9535f948e0113dda5984f224c2c378ce61b7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-04 10:45:14 +0200

    feat: add user validation using express-validator

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,3 +1,28 @@
+const { body, validationResult } = require('express-validator');
+
+const errNameLength = 'should have a length between 2 and 25 characters.';
+const errEmailLength = 'Email should have a maximum length of 50 characters.';
+const errEmail = 'Email need to be a valid email.';
+
+const userValidation = [
+  body('first_name')
+    .trim()
+    .blacklist('<>')
+    .isLength({ min: 2, max: 25 })
+    .withMessage(`First name ${errNameLength}`),
+  body('last_name')
+    .trim()
+    .blacklist('<>')
+    .isLength({ min: 2, max: 25 }.withMessage(`Last name ${errNameLength}`)),
+  body('email')
+    .trim()
+    .blacklist('<>')
+    .isLength({ max: 50 })
+    .withMessage(errEmailLength)
+    .isEmail()
+    .withMessage(errEmail),
+];
+
 function getSignup(req, res) {
   res.render('sign-up');
 }
