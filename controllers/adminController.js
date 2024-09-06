d3934b84d449ccf76787d7fe4bdc2649791735e6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-06 09:36:29 +0200

    feat: add getAdmin and postAdmin functions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,19 @@
-const getAdmin = () => {};
+require('dotenv').config();
+const db = require('../db/queries');
 
-const postAdmin = () => {};
+const getAdmin = (req, res) => {
+  if (!req.user || !req.user.member || req.user.admin) {
+    return res.redirect('/');
+  }
+  res.render('admin');
+};
+
+const postAdmin = async (req, res) => {
+  if (req.body.password === process.env.ADMIN) {
+    await db.updateAdminStatus(req.user);
+    return res.redirect('/');
+  }
+  res.render('admin', { error: 'This is not the expected password.' });
+};
 
 module.exports = { getAdmin, postAdmin };
